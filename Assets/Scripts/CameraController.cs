22e7401db327cda6b39321bcac46b5f45af57cea
Jakub Nowak <jakub_nowak@interia.pl>
2021-11-21 20:27:17 +0100

    Add camera follow

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -6,13 +6,29 @@ public class CameraController : MonoBehaviour
 {
     public GameObject mainCamera;
 
+    public Transform target;
+    public Vector3 offset;
+    public float smoothSpeed = 0.125f;
+
+    private void LateUpdate()
+    {
+        CameraFollow();
+    }
+
     void CameraSet180()
     {
-        mainCamera.transform.Rotate(5.07f, 0, 180);
+        mainCamera.transform.Rotate(-5.07f, 0, 180);
     }
 
     void CameraSet0()
     {
         mainCamera.transform.Rotate(5.07f, 0, 0);
+    }
+
+    void CameraFollow()
+    {
+        Vector3 desiredPosition = target.position + offset;
+        Vector3 smoothPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
+        transform.position = smoothPosition;
     }
 }
